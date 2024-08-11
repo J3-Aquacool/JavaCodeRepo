6b8472af4eb082ade8dbb7b5a710a0e7fe4033fa
J3-Aquacool <jagdish.java2@gmail.com>
2024-08-11 13:02:35 +0530

    Jenkins polling

--- MavenGithubJenkins/src/main/java/com/demo/App.java
+++ MavenGithubJenkins/src/main/java/com/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Iam integerated with Github" );
+        System.out.println( "Iam still integerated with Github now jenkins" );
     }
 }
