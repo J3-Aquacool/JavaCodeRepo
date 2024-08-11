454d6ef77453d84e038df0f6892e8e1a8cd462f8
J3-Aquacool <jagdish.java2@gmail.com>
2024-08-11 11:25:08 +0530

    Added Java Code File

---
+++ JavaGitHubConsoleApp/src/JavaConsoleRepoApp.java
@@ -0,0 +1,11 @@
+
+public class JavaConsoleRepoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Iam Integerated to Git and Github");
+	}
+
+}
