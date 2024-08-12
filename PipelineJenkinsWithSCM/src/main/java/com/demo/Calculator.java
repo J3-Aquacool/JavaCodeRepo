806b4e9e2fe90a824caac062bb25426b56a9ae2d
J3-Aquacool <jagdish.java2@gmail.com>
2024-08-12 20:25:23 +0530

    added test case

---
+++ PipelineJenkinsWithSCM/src/main/java/com/demo/Calculator.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class Calculator {
+	
+	public int add(int a,int b)
+	{
+		
+		return  +b;
+	}
+
+}
---
+++ PipelineJenkinsWithSCM/src/test/java/com/demo/MathAppTest.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathAppTest {
+
+	@Test
+	public void testAdd() {
+		
+		Calculator cd=new Calculator();
+		assertEquals(20,cd.add(10, 10));
+	}
+
+}
